583b35f210d5ff170ca858d9ef65499ac1ea27c4
gtindo <gtyletindo@gmail.com>
2024-10-12 15:32:30 +0200

    feat: add script to generate qr codes from a hard coded list of content

---
+++ scripts/generate_qr_codes.ts
@@ -0,0 +1,49 @@
+import { qrcode } from "jsr:@libs/qrcode";
+import * as path from "@std/path";
+import * as fs from "@std/fs";
+
+
+const CONTENT_TO_GENERATE = [
+  {name: "lib.svg", text: "A library"},
+  {name: "website.svg", text: "https://gtindo.dev"},
+  {name: "acronym.svg", text: "G_T_Y"},
+  {name: "one_piece.svg", text: "One piece"},
+  {name: "naruto.svg", text: "Naruto"}
+]
+
+
+function generateQrCode(text: string): string {
+  return qrcode(text, { output: "svg" });
+}
+
+async function saveFile(path: string, content: string) {
+  const encoder = new TextEncoder();
+  await Deno.writeFile(path, encoder.encode(content), {create: true});
+}
+
+
+async function main() { 
+  const OUTPUT_DIR_NAME = "outputs";
+  
+  // The posix file url representation of the current dir
+  const currentDirUrl = path.dirname(import.meta.url);
+  const outputDirUrl = `${currentDirUrl}/${OUTPUT_DIR_NAME}`;
+
+  // create the directory if it does not exists
+  await fs.ensureDir(path.fromFileUrl(outputDirUrl));
+
+  for(const content of CONTENT_TO_GENERATE) {
+    console.info(`Generating qrcode for ${content.name}`);
+
+    const svgContent = generateQrCode(content.text);
+    
+    // Actual path on the file system of running os
+    const outputPath = path.fromFileUrl(`${outputDirUrl}/${content.name}`);
+
+    await saveFile(outputPath, svgContent);
+  }
+
+  console.info("All Done!");
+}
+
+await main();
